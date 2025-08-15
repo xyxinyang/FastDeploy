8f9b6bbfe13d1942a1ec1426affdde6da8ad5329
wangyafeng <wangyafeng@baidu.com>
2025-08-15 17:51:38 +0800

    adapt qwen2_5_vl processor v2

--- fastdeploy/input/preprocess.py
+++ fastdeploy/input/preprocess.py
@@ -99,7 +99,6 @@ class InputPreprocessor:
                 )
         else:
             if ErnieArchitectures.contains_ernie_arch(architectures):
-                # raise ValueError(f"Model {self.model_name_or_path} is not a valid Ernie4_5_VL model.")
                 from fastdeploy.input.ernie_vl_processor import ErnieMoEVLProcessor
 
                 self.processor = ErnieMoEVLProcessor(
@@ -118,7 +117,5 @@ class InputPreprocessor:
                     limit_mm_per_prompt=self.limit_mm_per_prompt,
                     mm_processor_kwargs=self.mm_processor_kwargs,
                     reasoning_parser_obj=reasoning_parser_obj,
-                    tool_parser_obj=tool_parser_obj,
                 )
-
         return self.processor
